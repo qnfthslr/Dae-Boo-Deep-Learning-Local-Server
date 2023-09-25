e5a910b5300a9d508c5e73115da7ec496b59f576
qnfthslr <ojs2354@naver.com>
2023-09-25 16:57:54 +0900

    Fix: [이상원] Deep Learning Framework가 list 형태의 응답 결과를 보낼 때 엔코딩을 제대로 못하는 문제 [PMDLLS-10]

--- inference/age_model.py
+++ inference/age_model.py
@@ -5,11 +5,13 @@ from tensorflow import keras
 from deep_learn.age.learning_processor import AgeBasedLearningProcessor
 
 class AgeBasedinferringProcessor:
+    # def __init__(self,data):
     def __init__(self):
         self.loaded_model = load_model(os.path.join("./data/model", 'age_based_card_recommand.h5'))
         self.columns = ['age', 'card_id']
         self.output_names = ['age_map_data.xlsx', 'card_number_map_data.xlsx']
         self.learning_processor = AgeBasedLearningProcessor(self.columns, self.output_names)
+        # self.data = data
 
     def process_data(self, file_path):
         self.learning_processor.set_file_path(file_path)
@@ -26,18 +28,15 @@ class AgeBasedinferringProcessor:
         print(f"predicted_age_index: {predicted_age_index}")
 
         age_data = np.array([request_columns_map_data[0][20]])
+        # age_data = np.array([request_columns_map_data[0][self.data]])
         predicted_probs = self.loaded_model.predict(age_data)[0]
         recommended_card_indices = np.argsort(predicted_probs)[::-1][:5]
         recommended_card_numbers = [list(request_columns_map_data[1].keys())[i] for i in recommended_card_indices]
         print(f"recommended_card_numbers: {recommended_card_numbers}")
 
-        age_data = np.array([request_columns_map_data[0][30]])
-        predicted_probs = self.loaded_model.predict(age_data)[0]
-        recommended_card_indices = np.argsort(predicted_probs)[::-1][:5]
-        recommended_card_numbers = [list(request_columns_map_data[1].keys())[i] for i in recommended_card_indices]
-        print(f"recommended_card_numbers: {recommended_card_numbers}")
+        return recommended_card_numbers
 
-        return 1
+
 
 if __name__ == "__main__":
     processor = AgeBasedinferringProcessor()
